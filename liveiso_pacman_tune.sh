fc5ad755e33104e2df44852fa4e7f4ed670e5c55
Gene <crocogene@gmail.com>
2025-09-16 16:47:26 +0000

    Update liveiso_pacman_tune.sh

--- liveiso_pacman_tune.sh
+++ liveiso_pacman_tune.sh
@@ -3,6 +3,7 @@
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 pacman -S --noconfirm  pacman-contrib gnupg
+gpg --list-keys #gnupg init
 pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key F3B607488DB35A47 
 pacman -U --noconfirm \
