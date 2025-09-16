f3a4c8688819aa64bba4146d90fd680d9c0e89f8
Gene <crocogene@gmail.com>
2025-09-16 16:33:07 +0000

    Create liveiso_pacman_tune.sh

---
+++ liveiso_pacman_tune.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pacman -S --noconfirm  pacman-contrib 
+pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
+pacman-key --lsign-key F3B607488DB35A47 
+pacman -U --noconfirm \
+'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-keyring-20240331-1-any.pkg.tar.zst' \
+'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-mirrorlist-22-1-any.pkg.tar.zst' \
+'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v4-mirrorlist-22-1-any.pkg.tar.zst' 
+cp "$SCRIPT_DIR/pacman_liveiso.conf" /etc/pacman.conf
+pacman -Syu
