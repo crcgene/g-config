dadee4e1ea5e520e40f9f111008455dc4629da28
Gene <crocogene@gmail.com>
2025-09-16 17:05:26 +0000

    Update liveiso_pacman_tune.sh

--- liveiso_pacman_tune.sh
+++ liveiso_pacman_tune.sh
@@ -1,19 +1,17 @@
 #!/usr/bin/env bash
 
-printf '$0 = %s\n' "$0"
-printf 'BASH_SOURCE[0] = %s\n' "${BASH_SOURCE[0]}"
+[ "$EUID" -eq 0 ] && { echo "Don't run uder sudo/root."; exit 1; }
 
 SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]:-$0}")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
-echo "SCRIPT_DIR = $SCRIPT_DIR"
 
-pacman -S --noconfirm  pacman-contrib gnupg
-gpg --list-keys #gnupg init
-pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
-pacman-key --lsign-key F3B607488DB35A47 
-pacman -U --noconfirm \
+sudo pacman -S --noconfirm  pacman-contrib gnupg
+sudo gpg --list-keys #gnupg init
+sudo pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key F3B607488DB35A47 
+sudo pacman -U --noconfirm \
 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-keyring-20240331-1-any.pkg.tar.zst' \
 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-mirrorlist-22-1-any.pkg.tar.zst' \
 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v4-mirrorlist-22-1-any.pkg.tar.zst' 
-cp "$SCRIPT_DIR/pacman_liveiso.conf" /etc/pacman.conf
-pacman -Syu
+sudo cp "$SCRIPT_DIR/pacman_liveiso.conf" /etc/pacman.conf
+sudo pacman -Syu
