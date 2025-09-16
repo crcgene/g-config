e7fdb7c318b7a25970dbc4750a7e416d5530fe01
Gene <crocogene@gmail.com>
2025-09-16 20:56:30 +0000

    Update and rename pacman_cachyos.sh to add_cachy_repos.sh

--- add_cachy_repos.sh
+++ add_cachy_repos.sh
@@ -6,12 +6,11 @@ SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]:-$0}")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
 
 sudo pacman -S --noconfirm  pacman-contrib gnupg
-sudo gpg --list-keys #gnupg init
 sudo pacman-key --recv-keys F3B607488DB35A47 --keyserver keyserver.ubuntu.com
 sudo pacman-key --lsign-key F3B607488DB35A47 
 sudo pacman -U --noconfirm \
 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-keyring-20240331-1-any.pkg.tar.zst' \
 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-mirrorlist-22-1-any.pkg.tar.zst' \
 'https://mirror.cachyos.org/repo/x86_64/cachyos/cachyos-v4-mirrorlist-22-1-any.pkg.tar.zst' 
-sudo cp "$SCRIPT_DIR/pacman_liveiso.conf" /etc/pacman.conf
+sudo cp "$SCRIPT_DIR/pacman_with_cachy.conf" /etc/pacman.conf
 sudo pacman -Syu
